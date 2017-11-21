25eec7e611024fa28f098f5be9f2cd276b77c907
Chris Gonzales <cm.gonzales@yahoo.com>
2017-11-20 19:18:34 -0800

    added css

--- components/Self.js
+++ components/Self.js
@@ -24,14 +24,18 @@ class Self extends React.Component {
        
     
           
-         <div>          
-            <h1>{this.state.title}!</h1>
+         <div>   
+            <header><h1>{this.state.title}!</h1></header>  
+            <main>     
+            
             <img src={require('./one.jpg')} />
             <h3>What is a STATE?</h3>  <p> <br/> an object that determines how that component renders & behaves. In other words, “state” is what allows you to create components that are dynamic and interactive</p>
             <h3>What is Props </h3> <p> <br/>Most components can be customized when they are created, with different parameters. These creation parameters are called props</p>        
             <h3>What is ReactDOM</h3><br/><p> serves as the entry point of the DOM-related rendering paths. It is intended to be paired with the isomorphic React, which will be shipped as react to npm.</p>
             <br/>
             <button onClick= {this.buttonClicked}   >click me</button>
+            </main>
+            <footer>Iam a footer</footer>
             </div>
             
             
